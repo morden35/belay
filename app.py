68b478c89349b441a6b588b680ce20b8819dc74c
Michelle Orden <michelleorden@Michelles-MacBook-Pro.local>
2022-03-06 16:42:43 -0600

    allow users to create username and password and see chats page. need to check login functionality

--- app.py
+++ app.py
@@ -12,20 +12,20 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # either holding it in memory on your server or in a sqlite database.
 # You may use the sample structures below or create your own.
 
-# def newChat(host_auth_key):
-#     magic_passphrase = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
-#     chat = {"authorized_users": set(),
-#             "magic_passphrase": magic_passphrase,
-#             "magic_link": "http://127.0.0.1:5000/chat/",
-#             "messages": []}
-#     chat["authorized_users"].add(host_auth_key)
-#     return chat
+def newChat(host_auth_key):
+    magic_passphrase = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
+    chat = {"authorized_users": set(),
+            "magic_passphrase": magic_passphrase,
+            "magic_link": "http://127.0.0.1:5000/chat/",
+            "messages": []}
+    chat["authorized_users"].add(host_auth_key)
+    return chat
 
 
-# chats = {}
-# unique_usernames = set()
-# usernames_to_keys = {}
-# users = {}
+chats = {}
+unique_usernames = set()
+usernames_to_keys = {}
+users = {}
 
 # TODO: Include any other routes your app might send users to
 @app.route('/')
@@ -70,84 +70,84 @@ def auth_user():
     jsonify({'success': False})
 
 
-@app.route('/update_user', methods=['POST'])
-def update_user():
-    data = json.loads(request.data)
-    auth_key = data['auth_key']
-    chat_id = data['chat_id']
-    chats[chat_id]["authorized_users"].add(auth_key)
-    users[auth_key]['chats'].append(chat_id)
-    return jsonify({'success': True})
+# @app.route('/update_user', methods=['POST'])
+# def update_user():
+#     data = json.loads(request.data)
+#     auth_key = data['auth_key']
+#     chat_id = data['chat_id']
+#     chats[chat_id]["authorized_users"].add(auth_key)
+#     users[auth_key]['chats'].append(chat_id)
+#     return jsonify({'success': True})
 
 
-@app.route('/join', methods=['POST'])
-def join():
-    data = json.loads(request.data)
-    auth_key = data['auth_key']
-    chat_id = data['chat_id']
-    magic_key = data['magic_key']
+# @app.route('/join', methods=['POST'])
+# def join():
+#     data = json.loads(request.data)
+#     auth_key = data['auth_key']
+#     chat_id = data['chat_id']
+#     magic_key = data['magic_key']
 
-    if (auth_key in users.keys() and
-        chat_id in chats.keys() and
-        chats[chat_id]['magic_passphrase'] == magic_key):
-        chats[chat_id]["authorized_users"].add(auth_key)
-        users[auth_key]["chats"].append(chat_id)
-        return jsonify({'success': True})
-    return jsonify({'success': False})
-
-
-@app.route('/load_chats', methods=['POST'])
-def load_chats():
-    data = json.loads(request.data)
-    auth_key = data['auth_key']
-    chats = users[auth_key]['chats']
-
-    return jsonify({'chats': chats})
+#     if (auth_key in users.keys() and
+#         chat_id in chats.keys() and
+#         chats[chat_id]['magic_passphrase'] == magic_key):
+#         chats[chat_id]["authorized_users"].add(auth_key)
+#         users[auth_key]["chats"].append(chat_id)
+#         return jsonify({'success': True})
+#     return jsonify({'success': False})
 
 
-@app.route('/create_chat', methods=['POST'])
-def create_chat():
-    data = json.loads(request.data)
-    auth_key = data['auth_key']
+# @app.route('/load_chats', methods=['POST'])
+# def load_chats():
+#     data = json.loads(request.data)
+#     auth_key = data['auth_key']
+#     chats = users[auth_key]['chats']
 
-    if auth_key in users.keys():
-        chat_id = str(len(chats))
-        new_chat = newChat(auth_key)
-        chats[chat_id] = new_chat
-        chats[chat_id]["magic_link"] = chats[chat_id]["magic_link"] + chat_id + "?magic_key=" + chats[chat_id]["magic_passphrase"] + "&chat_id=" + chat_id
-        users[auth_key]['chats'].append(chat_id)
-
-        return jsonify({'chat_id': chat_id,
-                        'magic_link': chats[chat_id]["magic_link"],
-                        'success': True})
-    return jsonify({'success': False})
+#     return jsonify({'chats': chats})
 
 
-@app.route('/get_magic_link', methods=['POST'])
-def get_magic_link():
-    data = json.loads(request.data)
-    chat_id = data['chat_id']
-    magic_link = chats[chat_id]["magic_link"]
-    return jsonify({'magic_link': magic_link})
+# @app.route('/create_chat', methods=['POST'])
+# def create_chat():
+#     data = json.loads(request.data)
+#     auth_key = data['auth_key']
+
+#     if auth_key in users.keys():
+#         chat_id = str(len(chats))
+#         new_chat = newChat(auth_key)
+#         chats[chat_id] = new_chat
+#         chats[chat_id]["magic_link"] = chats[chat_id]["magic_link"] + chat_id + "?magic_key=" + chats[chat_id]["magic_passphrase"] + "&chat_id=" + chat_id
+#         users[auth_key]['chats'].append(chat_id)
+
+#         return jsonify({'chat_id': chat_id,
+#                         'magic_link': chats[chat_id]["magic_link"],
+#                         'success': True})
+#     return jsonify({'success': False})
 
 
-@app.route('/post_message', methods=['POST'])
-def post_message():
-    data = json.loads(request.data)
-    auth_key = data['auth_key']
-    chat_id = data['chat_id']
-    text = data['text']
-    if auth_key in chats[chat_id]["authorized_users"]:
-        chats[chat_id]["messages"].append({"user": auth_key,
-                                            "text": text,
-                                            "author": users[auth_key]['username']})
-        return jsonify({'success': True})
-    return jsonify({'success': False})
+# @app.route('/get_magic_link', methods=['POST'])
+# def get_magic_link():
+#     data = json.loads(request.data)
+#     chat_id = data['chat_id']
+#     magic_link = chats[chat_id]["magic_link"]
+#     return jsonify({'magic_link': magic_link})
 
 
-@app.route('/get_messages', methods=['POST'])
-def get_messages():
-    data = json.loads(request.data)
-    chat_id = data['chat_id']
-    messages = chats[chat_id]['messages'][-30:]    
-    return {"messages": messages}
+# @app.route('/post_message', methods=['POST'])
+# def post_message():
+#     data = json.loads(request.data)
+#     auth_key = data['auth_key']
+#     chat_id = data['chat_id']
+#     text = data['text']
+#     if auth_key in chats[chat_id]["authorized_users"]:
+#         chats[chat_id]["messages"].append({"user": auth_key,
+#                                             "text": text,
+#                                             "author": users[auth_key]['username']})
+#         return jsonify({'success': True})
+#     return jsonify({'success': False})
+
+
+# @app.route('/get_messages', methods=['POST'])
+# def get_messages():
+#     data = json.loads(request.data)
+#     chat_id = data['chat_id']
+#     messages = chats[chat_id]['messages'][-30:]    
+#     return {"messages": messages}
--- static/script.js
+++ static/script.js
@@ -1,41 +1,121 @@
 class Belay extends React.Component {
+	// contains the entire app
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: null,
+			password: null,
+			auth_key: null,
+			isAuth: false,
+		}
+	}
+
 	render() {
-		return (
-			<div>
-				<h1>Belay</h1>
-				<Login/>
+		if (this.state.isAuth) {
+			return (
 				<Channels/>
-			</div>
+			);
+		}
+		return (
+			<Login
+			 createUser={() => this.createUsername()}
+			 loginUser={() => this.createUsername()}/>
 		);
+	}
+	
+	createUsername() {
+		console.log("Creating new user");
+		let username = document.querySelector("input#username").value;
+		let password = document.querySelector("input#password").value;
+	
+		if (username && password) {
+			let request = fetch("http://127.0.0.1:5000/create_user",
+								{method: 'POST',
+								 body: JSON.stringify({'username': username,
+													   'password': password})});
+			request.then((response) => response.json())
+			.then(data => {
+				if (data['success']) {
+					let auth_key = data['auth_key'];
+					this.setState({username: username,
+								   password: password,
+								   auth_key: auth_key,
+								   isAuth: true});
+				}
+				else {
+					console.log("Username is unavailable. Please enter a valid username and password.");
+				}
+			});
+		}
+	}
+
+	login() {
+		let username = document.querySelector("input#username").value;
+		let password = document.querySelector("input#password").value;
+	
+		if (username && password) {
+			let request = fetch("http://127.0.0.1:5000/auth_user",
+								{method: 'POST',
+								 body: JSON.stringify({'username': username,
+														'password': password})});
+			request.then((response) => response.json())
+			.then(data => {
+				if (data['success']) {
+					let auth_key = data['auth_key'];
+					this.setState({username: username,
+						password: password,
+						auth_key: auth_key,
+						isAuth: true});
+				}
+				else {
+					console.log("Please enter a valid username and password.");
+				}
+			});
+		}
 	}
 }
 
 class Login extends React.Component {
+	// displays login page
 	render() {
 		return (
 			<div className="auth container">
-				<h2>Welcome to Belay!</h2>
-				<h3>Create a new account:</h3>
-				<p>Enter a username</p>
-				<input id="new_username"></input>
-				<p>Enter a password</p>
-				<input id="new_password"></input>
-				<button>Update</button>
-				<h3>Login with existing account:</h3>
-				<p>Enter a username</p>
-				<input id="old_username"></input>
-				<p>Enter a password</p>
-				<input id="old_password"></input>
-				<button>Update</button>
+				<h1>Welcome to Belay!</h1>
+				<h2>Login</h2>
+				<div>
+					<p>Enter a username</p>
+					<input id="username"></input>
+					<p>Enter a password</p>
+					<input id="password"></input>
+				</div>
+				<div id="buttons">
+					<button id="login" onClick={() => this.props.loginUser()}>Log In</button>
+					<button id="create" onClick={() => this.props.createUser()}>Create Account</button>
+				</div>
 			</div>
 		);
 	}
 }
 
 class Channels extends React.Component {
+	// displays available channels
 	render() {
 		return (
-			<h1>Join a Channel</h1>
+			<div>
+				<h1>Belay</h1>
+				<div id="channels">
+					<div id="join">
+						<h2>Join a Channel</h2>
+						<ul>
+							<li>Channel 1</li>
+							<li>Channel 2</li>
+						</ul>
+					</div>
+					<div id="chat">
+						<h2>Chat chat chat</h2>
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
